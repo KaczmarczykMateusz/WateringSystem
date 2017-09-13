e4cb86621fb12ac5bc208a417adf9f3fd616876a
KaczmarczykMateusz <kaczmarczyk.wsb@gmail.com>
2017-09-13 22:07:48 +0300

    Analog light sensor working 100% correct
    Sensor based on transoptor

--- analogLightSensor.c
+++ analogLightSensor.c
@@ -4,12 +4,26 @@
  Author      : Mateusz Kaczmarczyk
  Version     :
  Description : Strongest light possible shows result 4.01 Volt
+ 	 	 	 Calibrate with vatiable'maxValue'
  ============================================================================
  */
-
-float lightAsPercent(void) {
+#include "analogLightSensor.h"
+/*
+uint8_t lightAsPercent(void) {
 	float light = adcOversampleEfficient();
 	float onePercent = (float)100/8192;
 	light = light * onePercent;
-	return light;
+	return (uint8_t)light;
+}*/
+uint8_t lightSensor(TVOLT * voltage) {
+	light = voltage->adcVoltRaw;
+	maxValue = 454;
+	light = (1000000/maxValue) * light;
+	light /= 10000;
+
+	if(light > 100) {
+		light = 100;
+	}
+
+	return (uint8_t)light;
 }
--- analogLightSensor.h
+++ analogLightSensor.h
@@ -10,6 +10,13 @@
 #ifndef ANALOGLIGHTSENSOR_H_
 #define ANALOGLIGHTSENSOR_H_
 
-float lightAsPercent(void);
+#include "adc.h"
+#define LIGHT_SENS_MAX_OUT 400; // Maximum value which may be shown by sensor (drought) multiplied "*100" minus "-1"
+
+uint8_t lightAsPercent(void);
+uint8_t lightSensor(TVOLT * voltage);
+
+uint32_t maxValue;
+uint32_t light;
 
 #endif /* ANALOGLIGHTSENSOR_H_ */
