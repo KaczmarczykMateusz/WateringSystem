369977b50f6858b2827b1e449d435e11180bac79
KaczmarczykMateusz <kaczmarczyk.wsb@gmail.com>
2017-10-09 23:16:16 +0300

    comments added

--- lcd.c
+++ lcd.c
@@ -1,9 +1,20 @@
-//	lcd.c
-
+/*
+============================================================================
+Name        : lcd.c
+Author      : Mateusz Kaczmarczyk
+Version     : Microcontroller : Atmel AVR Atmega32, LCD HD44780
+Description : See header file
+============================================================================
+*/
 #include "lcd.h"
 
-// Send half byte
-void _LCD_HalfOut(unsigned char writeHalf) {
+/*************************************************************************
+ Function: 	_LCD_HalfOut()
+ Purpose:	Send half byte
+ Input:		First send older half of byte
+ see:		_LCD_Write()
+ **************************************************************************/
+void _LCD_HalfOut(uint8_t writeHalf) {
 if(writeHalf & 0x01)
 	LCD_DB4_PORT |= LCD_DB4;
 else
@@ -25,81 +36,111 @@ else
 	LCD_DB7_PORT  &= ~LCD_DB7;
 }
 
-// write entire byte (data or command)
-void _LCD_Write(unsigned char dataToWrite) {
+/*************************************************************************
+ Function: 	_LCD_Write()
+ Purpose:	Write entire byte (data or command)
+ Input:		Byte to write
+ **************************************************************************/
+void _LCD_Write(uint8_t dataToWrite) {
 	LCD_E_PORT |= LCD_E;
 	_LCD_HalfOut(dataToWrite >> 4);
 	LCD_E_PORT &= ~LCD_E;
 	LCD_E_PORT |= LCD_E;
-	_LCD_HalfOut(dataToWrite);
+	_LCD_HalfOut((dataToWrite & ~(0xF0)));
 	LCD_E_PORT &= ~LCD_E;
 	_delay_us(50);
 }
 
-// write command
-void LCD_WriteCommand(unsigned char commandToWrite) {
+/*************************************************************************
+ Function: 	LCD_WriteCommand()
+ Purpose:	Write command
+ Input:		Command byte only
+ **************************************************************************/
+void LCD_WriteCommand(uint8_t commandToWrite) {
 	LCD_RS_PORT &= ~LCD_RS;
 	_LCD_Write(commandToWrite);
 }
 
-// write data
-void LCD_WriteData(unsigned char dataToWrite) {
+/*************************************************************************
+ Function: 	_LCD_WriteData()
+ Purpose:	Write data
+ Input:		Data byte one by one
+ **************************************************************************/
+void _LCD_WriteData(uint8_t dataToWrite) {
 	LCD_RS_PORT |= LCD_RS;
 	_LCD_Write(dataToWrite);
 }
 
-// Write text
+/*************************************************************************
+ Function: 	LCD_WriteText()
+ Purpose:	Write text
+ Input:		Char array
+ **************************************************************************/
 void LCD_WriteText(char * text) {
 	while(*text)
-		LCD_WriteData(*text++);
+		_LCD_WriteData(*text++);
 }
 
-// Set starting position on display
-void LCD_GoTo(unsigned char x, unsigned char y) {
+/*************************************************************************
+ Function: 	LCD_GoTo()
+ Purpose:	Set from where to start writing at display
+ Input:		x: horizontal position
+ 	 	 	y: vertical position
+ **************************************************************************/
+void LCD_GoTo(uint8_t x, uint8_t y) {
 	LCD_WriteCommand(HD44780_DDRAM_SET | (x + (0x40 * y)));
 }
 
-// Clearing text from display
+/*************************************************************************
+ Function: 	LCD_Clear()
+ Purpose:	Remove text from display
+ **************************************************************************/
 void LCD_Clear(void) {
 	LCD_WriteCommand(HD44780_CLEAR);
 	_delay_ms(2);
 }
 
-// Set defaulut starting point at display
+/*************************************************************************
+ Function: 	LCD_Home()
+ Purpose:	Return home (left, top)
+ **************************************************************************/
 void LCD_Home(void) {
 	LCD_WriteCommand(HD44780_HOME);
 	_delay_ms(2);
 }
 
-//  display initialization
+/*************************************************************************
+ Function: 	LCD_Initalize()
+ Purpose:	Display initialization
+ **************************************************************************/
 void LCD_Initalize(void) {
-	unsigned char i;
-	LCD_DB4_DIR |= LCD_DB4; // Configuration of pinout
-	LCD_DB5_DIR |= LCD_DB5; 
-	LCD_DB6_DIR |= LCD_DB6; 
-	LCD_DB7_DIR |= LCD_DB7; 
-	LCD_E_DIR 	|= LCD_E;   
-	LCD_RS_DIR 	|= LCD_RS;  
-	_delay_ms(15); // wait for current stabilization 
-	LCD_RS_PORT &= ~LCD_RS; 
-	LCD_E_PORT &= ~LCD_E;  
+	uint8_t i;
+	LCD_DB4_DIR |= LCD_DB4;	// Configuration of pinout
+	LCD_DB5_DIR |= LCD_DB5;
+	LCD_DB6_DIR |= LCD_DB6;
+	LCD_DB7_DIR |= LCD_DB7;
+	LCD_E_DIR 	|= LCD_E;
+	LCD_RS_DIR 	|= LCD_RS;
+	_delay_ms(15); 			// wait for current stabilisation
+	LCD_RS_PORT &= ~LCD_RS;
+	LCD_E_PORT &= ~LCD_E;
 
 	for(i = 0; i < 3; i++) {
-		LCD_E_PORT |= LCD_E; //  E = 1
-		_LCD_HalfOut(0x03); // 8 bit mode
-		LCD_E_PORT &= ~LCD_E; // E = 0
-		_delay_ms(5); 
+		LCD_E_PORT |= LCD_E;	//  E = 1
+		_LCD_HalfOut(0x03);		// 8 bit mode
+		LCD_E_PORT &= ~LCD_E;	// E = 0
+		_delay_ms(5);
 	}
 
-	LCD_E_PORT |= LCD_E; // E = 1
-	_LCD_HalfOut(0x02); // 4 bit mode
-	LCD_E_PORT &= ~LCD_E; // E = 0
+	LCD_E_PORT |= LCD_E;	// E = 1
+	_LCD_HalfOut(0x02);		// 4 bit mode
+	LCD_E_PORT &= ~LCD_E;	// E = 0
 
-	_delay_ms(1); 
-	LCD_WriteCommand(HD44780_FUNCTION_SET | HD44780_FONT5x7 | HD44780_TWO_LINE | HD44780_4_BIT); // interfejs 4-bity, 2-linie, znak 5x7
-	LCD_WriteCommand(HD44780_DISPLAY_ONOFF | HD44780_DISPLAY_OFF); // turn off display
-	LCD_WriteCommand(HD44780_CLEAR); // clear DDRAM
+	_delay_ms(1);
+	LCD_WriteCommand(HD44780_FONT5x8 | HD44780_TWO_LINE | HD44780_4_BIT);				// 4 BIT interface, 2 lines, font size 5x7
+	LCD_WriteCommand(HD44780_DISPLAY_OFF);	// turn off display
+	LCD_WriteCommand(HD44780_CLEAR);		// clear DDRAM
 	_delay_ms(2);
-	LCD_WriteCommand(HD44780_ENTRY_MODE | HD44780_EM_SHIFT_CURSOR | HD44780_EM_INCREMENT);// address incrementation and shift of coursor
-	LCD_WriteCommand(HD44780_DISPLAY_ONOFF | HD44780_DISPLAY_ON | HD44780_CURSOR_OFF | HD44780_CURSOR_NOBLINK); // turn on display without coursr nor blinking
+	LCD_WriteCommand(HD44780_EM_RIGHT);		// address incrementation and shift of cursor
+	LCD_WriteCommand(HD44780_DISPLAY_ON | HD44780_CURSOR_OFF | HD44780_CURSOR_NOBLINK);	// turn on display without cursor nor blinking
 }
--- lcd.h
+++ lcd.h
@@ -1,12 +1,11 @@
-/*	lcd.c
- *
- *      Author: Mateusz Kaczmarczyk
- *      LCD display HD44780,
- *      4-bit mode without RW (Read/Write) flag
- *      Microcontroller : Atmel AVR Atmega
- *      16MHz external crystal resonator
- */
-
+/*
+============================================================================
+Name        : lcd.h
+Author      : Mateusz Kaczmarczyk
+Version     : Microcontroller : Atmel AVR Atmega32, LCD with driver HD44780
+Description : Should work on other AVR too (Atmega8 and 32 tested)
+============================================================================
+*/
 #ifndef LCD_H
 #define LCD_H
 
@@ -14,7 +13,7 @@
 #include <util/delay.h>
 
 
-// PORT configuration
+// @brief PORT configuration block
 #define LCD_RS_DIR		DDRB
 #define LCD_RS_PORT 	PORTB
 #define LCD_RS			(1 << PB0)
@@ -39,51 +38,75 @@
 #define LCD_DB7_PORT	PORTC
 #define LCD_DB7			(1 << PC4)
 
+// @brief Command definitions block
+#define HD44780_CLEAR				0x01	//@brief wipe entire display from what was displayed
 
-//  Command definitions HD44780
-#define HD44780_CLEAR					0x01
+#define HD44780_HOME				0x02	//@brief Return to top left corner of screen
 
-#define HD44780_HOME					0x02
+#define HD44780_EM_SHIFT_DECREMENT	(0x04 | 0x00)	//@brief Disable auto-scroll
+#define HD44780_EM_SHIFT_INCREMENT	(0x04 | 0x01)	//@brief Enable auto-scroll
+#define HD44780_EM_LEFT				(0x04 | 0x00)	//@brief Entry Mode (writing text) from right to right
+#define HD44780_EM_RIGHT			(0x04 | 0x02)	//@brief Entry Mode (writing text) from left to right
 
-#define HD44780_ENTRY_MODE				0x04
-	#define HD44780_EM_SHIFT_CURSOR		0
-	#define HD44780_EM_SHIFT_DISPLAY	1
-	#define HD44780_EM_DECREMENT		0
-	#define HD44780_EM_INCREMENT		2
+#define HD44780_DISPLAY_OFF			(0x08 | 0x00)	//@brief Turn OFF LCD display
+#define HD44780_DISPLAY_ON			(0x08 | 0x04)	//@brief Turn ON LCD display
+#define HD44780_CURSOR_OFF			(0x08 | 0x00)	//@brief Turn OFF cursor
+#define HD44780_CURSOR_ON			(0x08 | 0x02)	//@brief Turn ON cursor
+#define HD44780_CURSOR_NOBLINK		(0x08 | 0x00)	//@brief Solid cursor without blinking
+#define HD44780_CURSOR_BLINK		(0x08 | 0x01)	//@brief Cursor blinking mode
 
-#define HD44780_DISPLAY_ONOFF			0x08
-	#define HD44780_DISPLAY_OFF			0
-	#define HD44780_DISPLAY_ON			4
-	#define HD44780_CURSOR_OFF			0
-	#define HD44780_CURSOR_ON			2
-	#define HD44780_CURSOR_NOBLINK		0
-	#define HD44780_CURSOR_BLINK		1
+#define HD44780_SHIFT_CURSOR		(0x10 | 0x00)	//@brief move cursor to left or right (see below)
+#define HD44780_SHIFT_DISPLAY		(0x10 | 0x08)	//@brief move display to left or right (see below)
+#define HD44780_SHIFT_LEFT			(0x10 | 0x00)	//@brief choose to move display or cursor left
+#define HD44780_SHIFT_RIGHT			(0x10 | 0x04)	//@brief choose to move display or cursor right
 
-#define HD44780_DISPLAY_CURSOR_SHIFT	0x10
-	#define HD44780_SHIFT_CURSOR		0
-	#define HD44780_SHIFT_DISPLAY		8
-	#define HD44780_SHIFT_LEFT			0
-	#define HD44780_SHIFT_RIGHT			4
+#define HD44780_FONT5x8				(0x20 | 0x00)	//@brief Font size small
+#define HD44780_FONT5x10			(0x20 | 0x04)	//@brief Font size large
+#define HD44780_ONE_LINE			(0x20 | 0x00)	//@brief Display with only one row
+#define HD44780_TWO_LINE			(0x20 | 0x08)	//@brief Display with two rows
+#define HD44780_FOUR_LINE			(0x20 | 0x0F)	//@brief Display with four rows
 
-#define HD44780_FUNCTION_SET			0x20
-	#define HD44780_FONT5x7				0
-	#define HD44780_FONT5x10			4
-	#define HD44780_ONE_LINE			0
-	#define HD44780_TWO_LINE			8
-	#define HD44780_4_BIT				0
-	#define HD44780_8_BIT				16
+#define HD44780_4_BIT				(0x20 | 0x00)	//@brief 4-bit interface mode
+#define HD44780_8_BIT				(0x20 | 0x0F)	//@brief 8-bit interface mode
 
-#define HD44780_CGRAM_SET				0x40
+#define HD44780_CGRAM_SET			0x40
 
-#define HD44780_DDRAM_SET				0x80
+#define HD44780_DDRAM_SET			0x80
 
+/** @brief  Write to HD44780 data half byte by half byte while in 4 bit interface mode
+ *  @param  older half of byte to send
+ */
+void _LCD_HalfOut(uint8_t writeHalf);
 
-void LCD_WriteCommand(unsigned char);
-void LCD_WriteData(unsigned char);
+/** @brief  Sends byte to HD44780 using _LCD_HalfOut while 4-bit interface mode
+ *  @param  One byte to send (no matter data  or command)
+ */
+void _LCD_WriteData(uint8_t);
+
+/** @brief
+ *  @param  Single command which is always one byte
+ */
+void LCD_WriteCommand(uint8_t);
+
+/** @brief	Write text to the display
+ *  @param  String as char array coded in ASCII
+ */
 void LCD_WriteText(char *);
-void LCD_GoTo(unsigned char, unsigned char);
+
+/** @brief  Set position from which to start writing text
+ *  @param  x: horizontal position
+ 	 	 	y: vertical position
+ */
+void LCD_GoTo(uint8_t, uint8_t);
+
+/** @brief Clear everything what currently displayed at screen
+ */
 void LCD_Clear(void);
+
+// @brief Start writing text to display from the beginning of it (right top corner)
 void LCD_Home(void);
+
+// @brief Initialise HD44780 driver based display
 void LCD_Initalize(void);
 
 #endif
