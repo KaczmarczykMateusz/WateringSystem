3d2e7807b6f9c077809a67c5c1a0d291793627f3
KaczmarczykMateusz <kaczmarczyk.wsb@gmail.com>
2017-11-21 22:57:14 +0200

    SD card handling for development
    Not tested version

--- fat16.c
+++ fat16.c
@@ -1,19 +1,20 @@
-#ifdef DEBUG
-    #include <stdio.h>
-#endif
-
+#include <stdio.h>
 #include "fat16.h"
 
-unsigned char fat16_buffer[FAT16_BUFFER_SIZE];
+uint8_t fat16_buffer[FAT16_BUFFER_SIZE];
 Fat16State fat16_state;
 
-// Initialize the library - locates the first FAT16 partition,
-// loads the relevant part of its boot sector to calculate
-// values needed for operation, and finally positions the
-// file reading routines to the start of root directory entries
-char fat16_init() {
-    char i;
-    unsigned long root_start;
+/*************************************************************************
+ Function: fat16_init()
+ Purpose:  Initialize the library - locates the first FAT16 partition,
+ loads the relevant part of its boot sector to calculate
+ values needed for operation, and finally positions the
+ file reading routines to the start of root directory entries
+Return: 0 if success, negative value if failure
+**************************************************************************/
+int8_t fat16_init() {
+    int8_t i;
+    uint32_t root_start;
 
     fat16_seek(0x1BE);
 
@@ -26,8 +27,9 @@ char fat16_init() {
             break;
     }
 
-    if(i == 4) // none of the partitions were FAT16
+    if(i == 4) {// none of the partitions were FAT16
         return FAT16_ERR_NO_PARTITION_FOUND;
+    }
 
     fat16_state.fat_start = 512 * FAT16_part->start_sector; // temporary
 
@@ -39,11 +41,11 @@ char fat16_init() {
 
     fat16_state.fat_start += FAT16_boot->reserved_sectors * 512;
 
-    root_start = fat16_state.fat_start + (unsigned long)FAT16_boot->fat_size_sectors *
-        (unsigned long)FAT16_boot->number_of_fats * 512;
+    root_start = fat16_state.fat_start + (uint32_t)FAT16_boot->fat_size_sectors *
+        (uint32_t)FAT16_boot->number_of_fats * 512;
 
     fat16_state.data_start = root_start + sizeof(Fat16Entry) *
-        (unsigned long)FAT16_boot->root_dir_entries;
+        (uint32_t)FAT16_boot->root_dir_entries;
 
     fat16_state.sectors_per_cluster = FAT16_boot->sectors_per_cluster;
 
@@ -61,11 +63,16 @@ char fat16_init() {
     return 0;
 }
 
-// Assumes we are at the beginning of a directory and fat16_state.file_left
-// is set to amount of file entries. Reads on until a given file is found,
-// or no more file entries are available.
-char fat16_open_file(char *filename, char *ext) {
-    char i, bytes;
+/*************************************************************************
+ Function: fat16_open_file()
+ Purpose:  Assumes we are at the beginning of a directory and fat16_state.file_left
+ is set to amount of file entries. Reads on until a given file is found,
+ or no more file entries are available.
+ Input
+ Return:
+ **************************************************************************/
+int8_t fat16_open_file(int8_t *filename, int8_t *ext) {
+    int8_t i, bytes;
 
 #ifdef DEBUG
     printf("Trying to open file [%s.%s]\n", filename, ext);
@@ -74,25 +81,33 @@ char fat16_open_file(char *filename, char *ext) {
     do {
         bytes = fat16_read_file(sizeof(Fat16Entry));
 
-        if(bytes < sizeof(Fat16Entry))
+        if(bytes < sizeof(Fat16Entry)) {
             return FAT16_ERR_FILE_READ;
+        }
 
 #ifdef DEBUG
-        if(FAT16_entry->filename[0])
+        if(FAT16_entry->filename[0]) {
             printf("Found file [%.8s.%.3s]\n", FAT16_entry->filename, FAT16_entry->ext);
+        }
 #endif
 
-        for(i=0; i<8; i++) // we don't have memcmp on a MCU...
-            if(FAT16_entry->filename[i] != filename[i])
+        for(i=0; i<8; i++) {// we don't have memcmp on a MCU...
+            if(FAT16_entry->filename[i] != filename[i]) {
                 break;
-        if(i < 8) // not the filename we are looking for
+            }
+        }
+        if(i < 8) {// not the filename we are looking for
             continue;
+        }
 
-        for(i=0; i<3; i++) // we don't have memcmp on a MCU...
-            if(FAT16_entry->ext[i] != ext[i])
+        for(i=0; i<3; i++) {// we don't have memcmp on a MCU...
+            if(FAT16_entry->ext[i] != ext[i]) {
                 break;
-        if(i < 3) // not the extension we are looking for
+            }
+        }
+        if(i < 3) {// not the extension we are looking for
             continue;
+        }
 
 #ifdef DEBUG
         printf("File found at cluster %d!\n", FAT16_entry->starting_cluster);
@@ -100,7 +115,7 @@ char fat16_open_file(char *filename, char *ext) {
 
         // Initialize reading variables
         fat16_state.cluster = FAT16_entry->starting_cluster;
-        fat16_state.cluster_left = (unsigned long)fat16_state.sectors_per_cluster * 512;
+        fat16_state.cluster_left = (uint32_t)fat16_state.sectors_per_cluster * 512;
 
         if(FAT16_entry->filename[0] == 0x2E ||
            (FAT16_entry->attributes & 0x10)) { // directory
@@ -112,8 +127,8 @@ char fat16_open_file(char *filename, char *ext) {
         }
 
         // Go to first cluster
-        fat16_seek(fat16_state.data_start + (unsigned long)(fat16_state.cluster-2) *
-            (unsigned long)fat16_state.sectors_per_cluster * 512);
+        fat16_seek(fat16_state.data_start + (uint32_t)(fat16_state.cluster-2) *
+            (uint32_t)fat16_state.sectors_per_cluster * 512);
         return 0;
     } while(fat16_state.file_left > 0);
 
@@ -124,20 +139,28 @@ char fat16_open_file(char *filename, char *ext) {
     return FAT16_ERR_FILE_NOT_FOUND;
 }
 
-char fat16_read_file(char bytes) { // returns the bytes read
+/*************************************************************************
+ Function: fat16_read_file()
+ Purpose: Read content of SD card as text byte by byte
+ Input:	 Size of buffer for reading as number of bytes
+ Return: Bytes left to read
+ See:	 FAT16_BUFFER_SIZE
+ **************************************************************************/
+int8_t fat16_read_file(int8_t bytes) {
 #ifdef DEBUG
     //printf("fat16_read_file: Cluster %d, bytes left %d/%d\n", fat16_state.cluster, fat16_state.file_left, fat16_state.cluster_left);
 #endif
 
-    if(fat16_state.file_left == 0)
+    if(fat16_state.file_left == 0) {
         return 0;
+    }
 
     if(fat16_state.cluster_left == 0) {
-        fat16_seek(fat16_state.fat_start + (unsigned long)fat16_state.cluster*2);
+        fat16_seek(fat16_state.fat_start + (uint32_t)fat16_state.cluster*2);
         fat16_read(2);
 
         fat16_state.cluster = FAT16_ushort[0];
-        fat16_state.cluster_left = (unsigned long)fat16_state.sectors_per_cluster * 512;
+        fat16_state.cluster_left = (uint32_t)fat16_state.sectors_per_cluster * 512;
 
         if(fat16_state.cluster == 0xFFFF) { // end of cluster chain
             fat16_state.file_left = 0;
@@ -145,20 +168,22 @@ char fat16_read_file(char bytes) { // returns the bytes read
         }
 
         // Go to first cluster
-        fat16_seek(fat16_state.data_start + (unsigned long)(fat16_state.cluster-2) *
-            (unsigned long)fat16_state.sectors_per_cluster * 512);
+        fat16_seek(fat16_state.data_start + (uint32_t)(fat16_state.cluster-2) *
+            (uint32_t)fat16_state.sectors_per_cluster * 512);
 
 #ifdef DEBUG
         printf("Next cluster %d\n", fat16_state.cluster);
 #endif
     }
 
-    if(bytes > fat16_state.file_left)
+    if(bytes > fat16_state.file_left) {
         bytes = fat16_state.file_left;
-    if(bytes > fat16_state.cluster_left)
+    }
+    if(bytes > fat16_state.cluster_left) {
         bytes = fat16_state.cluster_left;
+    }
 
-    bytes = fat16_read(bytes);
+    fat16_read(bytes);
 
     fat16_state.file_left -= bytes;
     fat16_state.cluster_left -= bytes;
@@ -170,46 +195,65 @@ char fat16_read_file(char bytes) { // returns the bytes read
     return bytes;
 }
 
-void fat16_seek(unsigned long offset) {
+/*************************************************************************
+ Function: fat16_seek()
+ Purpose:  Go to specified offset. Next fat16_read should continue from here
+ Input:	   Relative linear offset to the start of the partition.
+ 	 	   Next fat16_read should continue from here
+ **************************************************************************/
+void fat16_seek(uint32_t offset) {
 	sd_sector = offset >> 9;
 	sd_pos = offset & 0x1FF;
 }
 
-char fat16_read(unsigned char bytes) {
+/*************************************************************************
+ Function: fat16_read()
+ Purpose:
+ Input:	Amount of bytes to read
+ **************************************************************************/
+void fat16_read(uint8_t bytes) {
 	SD_read(sd_sector, sd_pos, fat16_buffer, bytes);
-	sd_pos+=(unsigned short)bytes;
+	sd_pos += (uint16_t)bytes;
 
 	if(sd_pos == 512) {
 		sd_pos = 0;
 		sd_sector++;
 	}
-
-	return bytes;
 }
 
-char SD_init() {
-	char i;
+/*************************************************************************
+ Function: SD_init()
+ Purpose: Initialise SD  card
+ Return:  0 if success, negative value if failure
+ **************************************************************************/
+int8_t SD_init() {
+	int8_t i;
 
 	// ]r:10
 	CS_DISABLE();
-	for(i=0; i<10; i++) // idle for 1 bytes / 80 clocks
+	for(i=0; i<10; i++) {// idle for 1 bytes / 80 clocks
 		SPI_write(0xFF);
+	}
 
-	// [0x40 0x00 0x00 0x00 0x00 0x95 r:8] until we get "1"
-	for(i=0; i<10 && SD_command(0x40, 0x00000000, 0x95, 8) != 1; i++)
+	//Send clock pulses for maximum 80 cycles (“read” 10 bytes) by writing zero
+	//SD card should respond within this time with "1" indicating successful initialisation
+	for(i=0; i<10 && SD_command(0x40, 0x00000000, 0x95, 8) != 1; i++) {
 		_delay_ms(100);
+	}
 
-	if(i == 10) // card did not respond to initialization
+	if(i == 10) {// card did not respond to initialisation
 		return -1;
+	}
 
-	// CMD1 until card comes out of idle, but maximum of 10 times
+	//Send CMD1 until it returns 0 (it returns 1 while the card is busy)
 	for(i=0; i<10 && SD_command(0x41, 0x00000000, 0xFF, 8) != 0; i++)
 		_delay_ms(100);
 
-	if(i == 10) // card did not come out of idle
+	if(i == 10) {// card did not come out of idle
 		return -2;
+	}
 
-	// SET_BLOCKLEN to 512
+	// Set block length to 512
 	SD_command(0x50, 0x00000200, 0xFF, 8);
 
 	sd_sector = sd_pos = 0;
@@ -217,30 +261,43 @@ char SD_init() {
 	return 0;
 }
 
-// TODO: This function will not exit gracefully if SD card does not do what it should
-void SD_read(unsigned long sector, unsigned short offset, unsigned char * buffer, unsigned short len) {
-	unsigned short i, pos = 0;
+/*************************************************************************
+ Function: SD_read()
+ Purpose: Reading data from SD card into "*buffer"
+ Input:	Current sector being read
+		Offset within current sd_sector
+		Buffer in which we will be saving reads
+		Length of data unit as bits
+ // TODO: This function will not exit gracefully if SD card does not do what it should
+ **************************************************************************/
+void SD_read(uint32_t sector, uint16_t offset, uint8_t *buffer, uint16_t length) {
+	uint16_t i;
 
 	CS_ENABLE();
 	SPI_write(0x51);
-	SPI_write(sector>>15); // sector*512 >> 24
-	SPI_write(sector>>7);  // sector*512 >> 16
-	SPI_write(sector<<1);  // sector*512 >> 8
+	SPI_write(sector >> 15); // sector*512 >> 24
+	SPI_write(sector >> 7);  // sector*512 >> 16
+	SPI_write(sector << 1);  // sector*512 >> 8
 	SPI_write(0);          // sector*512
 	SPI_write(0xFF);
 
-	for(i=0; i<100 && SPI_write(0xFF) != 0x00; i++) {} // wait for 0
+	for(i=0; i<100 && SPI_write(0xFF) != 0x00; i++) {
+	} // wait for 0
 
-	for(i=0; i<100 && SPI_write(0xFF) != 0xFE; i++) {} // wait for data start
+	for(i=0; i<100 && SPI_write(0xFF) != 0xFE; i++) {
+	} // wait for data start
 
-	for(i=0; i<offset; i++) // "skip" bytes
+	for(i=0; i<offset; i++)  {// "skip" bytes
 		SPI_write(0xFF);
+	}
 
-	for(i=0; i<len; i++) // read len bytes
+	for(i=0; i<length; i++) { // read length bytes
 		buffer[i] = SPI_write(0xFF);
+	}
 
-	for(i+=offset; i<512; i++) // "skip" again
+	for(i+=offset; i<512; i++) {// "skip" again
 		SPI_write(0xFF);
+	}
 
 	// skip checksum
 	SPI_write(0xFF);
@@ -249,8 +306,14 @@ void SD_read(unsigned long sector, unsigned short offset, unsigned char * buffer
 	CS_DISABLE();
 }
 
-unsigned char SD_command(unsigned char cmd, unsigned long arg, unsigned char crc, unsigned char read) {
-	unsigned char i, buffer[32], ret = 0xFF;
+/*************************************************************************
+ Function: SD_command()
+ Purpose: Send command to SD card
+ Input: SD bus protocol command, dword argument, CRC byte, Number of bits for reading
+ Return: Slave (SD card) response, "1" while SDsuccessfull Card busy
+ **************************************************************************/
+uint8_t SD_command(uint8_t cmd, uint32_t arg, uint8_t crc, uint8_t read) {
+	uint8_t i, buffer[32], ret = 0xFF;
 
 	uartPuts("CMD ");
 	uwrite_hex(cmd);
@@ -263,16 +326,18 @@ unsigned char SD_command(unsigned char cmd, unsigned long arg, unsigned char crc
 	SPI_write(arg);
 	SPI_write(crc);
 
-	for(i=0; i<read; i++)
+	for(i=0; i<read; i++) {
 		buffer[i] = SPI_write(0xFF);
+	}
 
 	CS_DISABLE();
 
 	for(i=0; i<read; i++) {
 		uartPutc(' ');
 		uwrite_hex(buffer[i]);
-		if(buffer[i] != 0xFF)
+		if(buffer[i] != 0xFF) {
 			ret = buffer[i];
+		}
 	}
 
 	uartPuts("\r\n");
--- fat16.h
+++ fat16.h
@@ -1,131 +1,192 @@
+
+/*
+ ============================================================================
+ Name        :	fat16.h
+ Author      :	Mateusz Kaczmarczyk
+ Version     :
+ Description :
+ Notice		 : Ensure that packed attribute is working properly
+			   because there are various versions of it and after
+			   implementing wrong one compiler won't show warning
+ ============================================================================
+ */
+
 #ifndef __FAT16_H
 #define __FAT16_H
 
-#include <avr/io.h>
 #include <util/delay.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "spi.h"
 #include "uart.h"
 
-uint32_t sd_sector;
-uint8_t sd_pos;
-
-// Partition information, starts at offset 0x1BE
-typedef struct {
-    unsigned char first_byte;
-    unsigned char start_chs[3];
-    unsigned char partition_type;
-    unsigned char end_chs[3];
-    uint32_t start_sector;
-    uint32_t length_sectors;
-} __attribute((packed)) PartitionTable; // concern replacing "gcc_struct, __packed__" with only "packed"
-
-// Bytes omitted from the start of Fat16BootSectorFragment
-#define FAT16_BOOT_OFFSET 11
-
-// Partial FAT16 boot sector structure - non-essentials commented out
-typedef struct {
-    /*unsigned char jmp[3];
-    char oem[8];*/
-	uint8_t sector_size;
-    unsigned char sectors_per_cluster;
-    uint8_t reserved_sectors;
-    unsigned char number_of_fats;
-    uint8_t root_dir_entries;
-    uint8_t total_sectors_short; // if zero, later field is used
-    unsigned char media_descriptor;
-    uint8_t fat_size_sectors;
-    /*uint8_t sectors_per_track;
-    uint8_t number_of_heads;
-    uint32_t hidden_sectors;
-    uint32_t total_sectors_long;
-
-    unsigned char drive_number;
-    unsigned char current_head;
-    unsigned char boot_signature;
-    uint32_t volume_id;
-    char volume_label[11];
-    char fs_type[8];
-    char boot_code[448];
-    uint8_t boot_sector_signature;*/
-} __attribute((packed)) Fat16BootSectorFragment;
-
-// FAT16 file entry
-typedef struct {
-    unsigned char filename[8];
-    unsigned char ext[3];
-    unsigned char attributes;
-    unsigned char reserved[10];
-    uint8_t modify_time;
-    uint8_t modify_date;
-    uint8_t starting_cluster;
-    uint32_t file_size;
-} __attribute((packed)) Fat16Entry;
-
-// State data required by FAT16 library
-typedef struct {
-	uint32_t fat_start; // FAT start position
-	uint32_t data_start; // data start position
-   unsigned char sectors_per_cluster; // cluster size in sectors
-   uint8_t cluster; // current cluster being read
-   uint32_t cluster_left; // bytes left in current cluster
-   uint32_t file_left; // bytes left in the file being read
-} __attribute((packed)) Fat16State;
-
 #define FAT16_BUFFER_SIZE 32
 
-// Global variables for read data and library state
-extern unsigned char fat16_buffer[FAT16_BUFFER_SIZE];
-extern Fat16State fat16_state;
+//@brief: Bytes omitted from the start of Fat16BootSectorFragment
+#define FAT16_BOOT_OFFSET 11
 
-// Aliases for fat16_buffer in different formats
+//@brief: Aliases for fat16_buffer in different formats
 #define FAT16_part ((PartitionTable *)((void *)fat16_buffer))
 #define FAT16_boot ((Fat16BootSectorFragment *)((void *)fat16_buffer))
 #define FAT16_entry ((Fat16Entry *)((void *)fat16_buffer))
 #define FAT16_ushort ((uint8_t *)((void *)fat16_buffer))
 
-// The following functions need to be provided for the library
-
-// Go to specified offset. Next fat16_read should continue from here
-void fat16_seek(uint32_t offset);
-// Read <bytes> to fat16_buffer, and return the amount of bytes read
-char fat16_read(unsigned char bytes);
-
-// The following functions are provided by the library
-
-// Error codes for fat16_init()
+//@brief: Error codes for fat16_init()
 #define FAT16_ERR_NO_PARTITION_FOUND -1
 #define FAT16_ERR_INVALID_SECTOR_SIZE -2
 
-// Initialize the FAT16 library. Nonzero return value indicates
-// an error state. After init, the file stream is pointed to
-// the beginning of root directory entry.
-char fat16_init(); // nonzero return values indicate error state
-
-// Error codes for fat16_open_file()
+//@brief: Error codes for fat16_open_file()
 #define FAT16_ERR_FILE_NOT_FOUND -1
 #define FAT16_ERR_FILE_READ -2
 
-// Open a given file. Assumes that the stream is currently at
-// a directory entry and fat16_state.file_left contains the
-// amount of file entries available. Both conditions are satisfied
-// after fat16_init(), and also after opening a directory with this.
-char fat16_open_file(char *filename, char *ext);
+int32_t sd_sector;	//@brief: Holds address of current sector
+uint8_t sd_pos;		//@brief:
 
-// Read <bytes> from file. Returns the bytes actually read, and
-// 0 if end of file is reached. This method will automatically
-// traverse all clusters of a file using the file allocation table
-char fat16_read_file(char bytes);
+//@brief: Partition information,it usually starts at offset 0x1BE
+typedef struct {
+    uint8_t first_byte;		//@brief: 0x80 if active (bootable), 0 otherwise
+    uint8_t start_chs[3];	//@brief: Start of the partition in CHS-addressing
+    uint8_t partition_type;	//@brief: Type of the partition
+    uint8_t end_chs[3];		//@brief: End of the partition in CHS-addressing
+    uint32_t start_sector;	//@brief: Relative offset to the partition in sectors (LBA)
+    uint32_t length_sectors;//@brief: size of the partition in sectors
+} __attribute((packed)) PartitionTable; // TODO: concern replacing "gcc_struct, __packed__" with only "packed"
 
-//@Brief: Initialise SD (not SDHC) card
-char SD_init();
+//@brief: Partial FAT16 boot sector structure - non-essentials commented out
+typedef struct {
+#if 0
+    uint8_t jmp[3];				//@brief: Intel 80x86 jump instruction
+    int8_t oem[8];				//@brief: OEM name (not the volume name, see offset 0x02B)
+#endif
+    uint16_t sector_size;		//@brief: Size of single sector (usually 512 bytes)
+    uint8_t sectors_per_cluster;//@brief: Number of sectors per cluster
+    uint16_t reserved_sectors;	//@brief: Number of reserved sectors (including the boot sector)
+    uint8_t number_of_fats;		//@brief: Number of File Allocation Tables recorded on partition
+    uint16_t root_dir_entries;	//@brief: Number of directory entries in the root directory (N.A. for FAT32)
+    uint16_t total_sectors_short; //@brief: If zero, later field is used, Total number of sectors on the disk/partition,
+    							//or zero for disks/partitions bigger than 32 MiB (the field at offset 0x020 should then be used)
+    uint8_t media_descriptor;	//@brief: Media descriptor (bit #2 holds whether the disk is removable)
+    uint16_t fat_size_sectors;
 
-// TODO: This function will not exit gracefully if SD card does not do what it should
-void SD_read(unsigned long sector, unsigned short offset, unsigned char * buffer, unsigned short len);
+#if 0
+    uint16_t sectors_per_track;	//@brief: Number of sectors per track (cylinder), CHS addressing
+    uint16_t number_of_heads;	//@brief: Number of heads, CHS addressing
+    uint32_t hidden_sectors;	//@brief: Number of hidden sectors, which is the number of sectors before the boot sector (this field may be set to zero, though)
+    uint32_t total_sectors_long;//@brief: Total number of sectors on the disk/partition, if this is above 32 MiB (only valid if the field at offset 0x013 is zero)
 
-//@Brief: sends command to SD card
-unsigned char SD_command(unsigned char cmd, uint32_t arg, unsigned char crc, unsigned char read);
+    uint8_t drive_number;		//@brief: Drive number (this field is at offset 0x024 in FAT12/FAT16)
+    uint8_t current_head;		//@brief: Current head (internal to DOS; this field is at offset 0x025 in FAT12/FAT16)
+    uint8_t boot_signature;		//@brief: Boot signature, the value 0x29 indicates the three next fields are valid (this field is at offset 0x026 in FAT12/FAT16)
+    uint32_t volume_id;			//@brief: Volume ID (serial number; this field is at offset 0x027 in FAT12/FAT16)
+    int8_t volume_label[11];	//@brief: Volume label (this field is at offset 0x02b in FAT12/FAT16)
+    int8_t fs_type[8];			//@brief: File system type (this field is at offset 0x036 in FAT12/FAT16)
+    int8_t boot_code[448];		//@brief: Boot code (starts at offset 0x03e in FAT12/FAT16, and is 448 bytes)
+    uint16_t boot_sector_signature;	//@brief: Must be 0x55AA
+#endif
+} __attribute((packed)) Fat16BootSectorFragment;
+
+//@brief: FAT16 file entry
+typedef struct {
+    uint8_t filename[8];	//@brief: Name of file for reading
+    uint8_t ext[3];			//@brief: Extension of file for reading
+    uint8_t attributes;		//@brief:
+    uint8_t reserved[10];	//@brief:
+    uint16_t modify_time;	//@brief: Time file was modified last time
+    uint16_t modify_date;	//@brief: Date file was modified last time
+    uint16_t starting_cluster;	//@brief: Offset of starting cluster
+    uint32_t file_size;		//@brief: Total size of file as bytes
+} __attribute((packed)) Fat16Entry;
+
+//@brief: State data required by FAT16 library
+typedef struct {
+   uint32_t fat_start;		//@brief: FAT start position
+   uint32_t data_start;		//@brief: Data start position
+   uint8_t sectors_per_cluster;	//@brief: Cluster size in sectors
+   uint16_t cluster; 		//@brief: Current cluster being read
+   uint32_t cluster_left;	//@brief: Bytes left in current cluster
+   uint32_t file_left;		//@brief:: Bytes left in the file being read
+} __attribute((packed)) Fat16State;
+
+
+//@brief: Global variables for read data and library state
+extern uint8_t fat16_buffer[FAT16_BUFFER_SIZE];
+extern Fat16State fat16_state;
+
+
+/*
+		**Following functions need to be provided for the library**
+*/
+/** @brief	: Go to specified offset. Next fat16_read should continue from here
+ *  @param	: Relative linear offset to the start of the partition.. Next fat16_read should continue from here
+ *  @return	: None
+ */
+void fat16_seek(uint32_t offset);
+
+/** @brief	: Read <bytes> to fat16_buffer, and return the amount of bytes read
+ *  @param	: Number of bytes to read
+ */
+void fat16_read(uint8_t bytes);
+
+
+/*
+		**Following functions are provided by the library**
+*/
+/** @brief	: Initialize the FAT16 library. Nonzero return value indicates
+ * 			an error state. After init, the file stream is pointed to
+ * 			the beginning of root directory entry.
+ *  @param	: None
+ *  @return	: 0: Success
+ *  		  FAT16_ERR_NO_PARTITION_FOUND
+ *  		  FAT16_ERR_INVALID_SECTOR_SIZE
+ */
+int8_t fat16_init(); // nonzero return values indicate error state
+
+/** @brief	: Open a given file. Assumes that the stream is currently at
+ * 			a directory entry and fat16_state.file_left contains the
+ * 			amount of file entries available. Both conditions are satisfied
+ * 			after fat16_init(), and also after opening a directory with this.
+ *  @param	:
+ *  @param	:
+ *  @return	: 0: Success
+ *  		  FAT16_ERR_FILE_NOT_FOUND
+ *  		  FAT16_ERR_FILE_READ
+ */
+int8_t fat16_open_file(int8_t *filename, int8_t *ext);
+
+/** @brief	: Read <bytes> from file. This method will automatically
+ * 			  traverse all clusters of a file using the file allocation table
+ *  @param	: Size of buffer as number of bytes
+ *  @return	: Bytes left to read
+ */
+int8_t fat16_read_file(int8_t bytes);
+
+/** @brief	: Initialise SD (not SDHC) card
+ *  @param	: None
+ *  @return	:  0: Success
+ *  		  -1: Card did not respond to initialisation
+ *  		  -2: Card did not come out of idle state
+ *  @See:	 FAT16_BUFFER_SIZE
+ */
+int8_t SD_init();
+
+/** @brief	: function to be used by fat16_read()
+ *  @param	sector	: Current sector being read
+ *  @param	offset	: Offset within current sd_sector
+ *  @param	buffer	: Buffer in which we will be saving reads
+ *  @param	length	: Length of data unit as bits
+ *  @return	: None
+ *  @see	: fat16_read()
+ *	TODO: This function will not exit gracefully if SD card does not do what it should
+ */
+void SD_read(uint32_t sector, uint16_t offset, uint8_t *buffer, uint16_t length);
+
+
+/** @brief  Sends command to SD card
+ *  @param  cmd	: SD bus protocol command
+ *  @param  arg	: argument dword
+ *  @param  crc	: CRC byte
+ *  @param  read: Number of bits for reading
+ *  @return	Slave (SD card) response,
+ 	 	 	1: SD card busy
+ */
+uint8_t SD_command(uint8_t cmd, uint32_t arg, uint8_t crc, uint8_t read);
 
 #endif
